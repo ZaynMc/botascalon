b91537508b3157f550e5e1e0a8ed47de52528693
ZaynMc <theobourrier@outlook.fr>
2019-04-07 09:35:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -423,7 +423,7 @@ function generateMessages(){
  
  
 bot.on("message", message => {
-    if (message.member.hasPermission("ADMINISTRATOR") && message.content.toLowerCase() == setupCMD){
+    if (message.content.toLowerCase() == setupCMD){
         var toSend = generateMessages();
         let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
         for (let mapObj of mappedArray){
