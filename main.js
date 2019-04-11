0c6c12a60374ee44e8f46ada129e97d4a236ea86
ZaynMc <theobourrier@outlook.fr>
2019-04-11 10:38:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -236,6 +236,35 @@ bot.on("message", async message => {
 
   // Part 1 : checking & removing the text
   //1 blacklisted words
+let blacklisted = ['http', 'https'] //words put , after the word
+
+//2 looking for words
+let foundInText = false;
+for (var i in blacklisted) { // loops through the blacklisted list
+  if(message.channel.name == "🔴pub-no-discord" || message.member.hasPermission("MANAGE_MESSAGES")) return;
+  
+
+  if (message.content.toLowerCase().includes(blacklisted[i].toLowerCase())) foundInText = true;
+}
+// checks casesensitive words
+
+//3 deletes and send message
+  if (foundInText) {
+    //message delete
+    message.delete();
+    //message send to author
+    message.author.send('La pub de discord sur le serveur ASCALON est interdite !');
+    //message channel send to author for advertissment
+    message.channel.send(`<@${message.author.id}> Bonjour, la pub de serveur discord est interdite. Dernière avertissement avant le ban`).then(message => message.delete(5000));
+
+  }
+});
+
+bot.on("message", async message => {
+  if(message.channel.type === "dm") return;
+
+  // Part 1 : checking & removing the text
+  //1 blacklisted words
 let blacklisted = ['discord.gg'] //words put , after the word
 
 //2 looking for words
